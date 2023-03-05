c5ddfc32a19d7ca71afe18410e493159a1cb5f2b
axelsr042000 <axelsaintromain@gmail.com>
2023-03-05 23:14:38 +0100

    Ajout EMF_Pj_1.py contenant summary

--- Code/EMF_Pj_1.py
+++ Code/EMF_Pj_1.py
@@ -31,7 +31,7 @@ data_source_df['Clean_DATE'] = pd.to_datetime(data_source_df['DATE'], format='%Y
 
 ########################################################################################################################
 """
-CREATION DATAFRAME POUR CALCUL DAILY RETURN
+CREATION DATAFRAME POUR CALCUL DAILY SIMPLE / LOG RETURN
 """
 daily_indices_df = data_source_df.copy()
 daily_indices_df['Clean_DATE'] = daily_indices_df.Clean_DATE.dt.to_period('D')
@@ -39,12 +39,14 @@ daily_indices_df = daily_indices_df.set_index('Clean_DATE')
 daily_indices_df = daily_indices_df.drop(columns=daily_indices_df.select_dtypes(exclude='number').columns)
 ########################################################################################################################
 """
-CALCUL DAILY RETURN
+CALCUL DAILY SIMPLE / LOG RETURN
 """
 # CREA DTF AVEC DECALAGE DE L'INDEX D'UN UNDICE
 lagged_day_indices_df = daily_indices_df.shift(1)
-# CALCULER LES RENDEMENTS QUOTIDIENS A L'AIDE DES VALEURS DECALEES
+# CALCULER LES RENDEMENTS QUOTIDIENS SIMPLES / LOG A L'AIDE DES VALEURS DECALEES
 indices_day_ret_df = (daily_indices_df - lagged_day_indices_df) / lagged_day_indices_df
+indices_log_day_ret_df = pd.DataFrame(np.log(daily_indices_df).values - np.log(lagged_day_indices_df).values,
+                                      index=daily_indices_df.index, columns=daily_indices_df.columns)
 """
 # VOIR SI NECESSAIRE DE SUPPRIMER PREMIERE LIGNE OU FIGURE NAN
 indices_ret_df = indices_ret_df.dropna()
@@ -61,7 +63,7 @@ indices_ret_df = pd.DataFrame(indices_ret_df, index=indices_df.index, columns=in
 
 ########################################################################################################################
 """
-CREATION DATAFRAME POUR CALCUL WEEKLY RETURN
+CREATION DATAFRAME POUR CALCUL WEEKLY SIMPLE / LOG RETURN
 """
 weekly_indices_df = data_source_df.copy()
 weekly_indices_df['Clean_DATE'] = weekly_indices_df.Clean_DATE.dt.to_period('W-FRI')
@@ -75,18 +77,26 @@ Weekly_indices_df = Weekly_indices_df.resample('W-FRI').last()
 '''
 ########################################################################################################################
 """
-CALCUL WEEKLY RETURN
+CALCUL WEEKLY SIMPLE / LOG RETURN
 """
 # CREA DTF AVEC DECALAGE DE L'INDEX D'UN UNDICE
 lagged_week_indices_df = weekly_indices_df.shift(1)
 # CALCULER LES RENDEMENTS hebdo A L'AIDE DES VALEURS DECALEES
 indices_week_ret_df = (weekly_indices_df - lagged_week_indices_df) / lagged_week_indices_df
+indices_log_week_ret_df = pd.DataFrame(np.log(weekly_indices_df).values - np.log(lagged_week_indices_df).values,
+                                       index=weekly_indices_df.index, columns=weekly_indices_df.columns)
 ########################################################################################################################
-'''
-LOG
-lagged_index = (indices_df.index + DateOffset(days=-1)).to_period('D')
-indices_df.index = indices_df.index.to_period('D')
-indices_ret_df = np.log(indices_df).values - np.log(indices_df.reindex(lagged_index)).values
-indices_ret_df = pd.DataFrame(indices_ret_df, index=indices_df.index, columns=indices_df.columns)
-'''
+
+########################################################################################################################
+"""
+SUMMARY STATISTICS AND CORRELATIONS
+"""
+sumstat_day_ret_df = indices_day_ret_df.describe()
+# print(sumstat_day_ret_df.to_latex())
+sumstat_log_day_ret_df = indices_log_day_ret_df.describe()
+# print(sumstat_log_day_ret_df.to_latex())
+sumstat_week_ret_df = indices_week_ret_df.describe()
+# print(sumstat_week_ret_df.to_latex())
+sumstat_log_week_ret_df = indices_log_week_ret_df.describe()
+# print(sumstat_log_week_ret_df.to_latex())
 ########################################################################################################################
