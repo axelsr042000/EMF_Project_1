f343dfd4dd8a5ef4a156e3c37ff8f2b5a9f405e4
axelsr042000 <axelsaintromain@gmail.com>
2023-03-09 21:29:59 +0100

    Version du code à utiliser

--- Code/EMF_Pj_1.py
+++ Code/EMF_Pj_1.py
@@ -7,14 +7,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import scipy.stats as stats
-from scipy.stats import chi2
+from scipy.stats import chi2, kurtosis, skew
 import math
 ########################################################################################################################
 """
 LOAD MODULES
 """
 from Code.Project_parameters import path
-# from Code.Project_functions import winsorize
 ########################################################################################################################
 """
 OPENDATA
@@ -42,9 +41,9 @@ CALCUL DAILY SIMPLE / LOG RETURN
 # CREA DTF AVEC DECALAGE DE L'INDEX D'UN UNDICE
 lagged_day_indices_df = daily_indices_df.shift(1)
 # CALCULER LES RENDEMENTS QUOTIDIENS SIMPLES / LOG A L'AIDE DES VALEURS DECALEES
-indices_day_ret_df = ((daily_indices_df - lagged_day_indices_df) / lagged_day_indices_df) * 100
-indices_log_day_ret_df = pd.DataFrame((np.log(daily_indices_df).values - np.log(lagged_day_indices_df).values)*100,
-                                      index=daily_indices_df.index, columns=daily_indices_df.columns)
+indices_day_ret_df = ((daily_indices_df - lagged_day_indices_df) / lagged_day_indices_df).dropna()
+indices_log_day_ret_df = pd.DataFrame((np.log(daily_indices_df).values - np.log(lagged_day_indices_df).values),
+                                      index=daily_indices_df.index, columns=daily_indices_df.columns).dropna()
 
 ########################################################################################################################
 
@@ -65,9 +64,9 @@ CALCUL WEEKLY SIMPLE / LOG RETURN
 # CREA DTF AVEC DECALAGE DE L'INDEX D'UN UNDICE
 lagged_week_indices_df = weekly_indices_df.shift(1)
 # CALCULER LES RENDEMENTS hebdo A L'AIDE DES VALEURS DECALEES
-indices_week_ret_df = ((weekly_indices_df - lagged_week_indices_df) / lagged_week_indices_df)*100
-indices_log_week_ret_df = pd.DataFrame((np.log(weekly_indices_df).values - np.log(lagged_week_indices_df).values)*100,
-                                       index=weekly_indices_df.index, columns=weekly_indices_df.columns)
+indices_week_ret_df = ((weekly_indices_df - lagged_week_indices_df) / lagged_week_indices_df).dropna()
+indices_log_week_ret_df = pd.DataFrame((np.log(weekly_indices_df).values - np.log(lagged_week_indices_df).values),
+                                       index=weekly_indices_df.index, columns=weekly_indices_df.columns).dropna()
 ########################################################################################################################
 
 ########################################################################################################################
@@ -87,16 +86,24 @@ VarlDR = np.var(indices_log_day_ret_df)
 VarlWR = np.var(indices_log_week_ret_df)
 
 # Skewness
-SksDR = indices_day_ret_df.skew()
-SksWR = indices_week_ret_df.skew()
-SklDR = indices_log_day_ret_df.skew()
-SklWR = indices_log_week_ret_df.skew()
+SksDR = pd.Series(skew(indices_day_ret_df, axis=0), index=indices_day_ret_df.columns)
+# indices_day_ret_df.skew()
+SksWR = pd.Series(skew(indices_week_ret_df, axis=0), index=indices_week_ret_df.columns)
+# indices_week_ret_df.skew()
+SklDR = pd.Series(skew(indices_log_day_ret_df, axis=0), index=indices_log_day_ret_df.columns)
+# indices_log_day_ret_df.skew()
+SklWR = pd.Series(skew(indices_log_week_ret_df, axis=0), index=indices_log_week_ret_df.columns)
+# indices_log_week_ret_df.skew()
 
-# Kurtosis
-KsDR = indices_day_ret_df.kurtosis()
-KsWR = indices_week_ret_df.kurtosis()
-KlDR = indices_log_day_ret_df.kurtosis()
-KlWR = indices_log_week_ret_df.kurtosis()
+# Kurtosis : fisher=False => Pearson's Kurtosis ; fisher=True => Fisher's Kurtosis = (Pearson's Kurtosis-3)
+KsDR = pd.Series(kurtosis(indices_day_ret_df, fisher=False, axis=0), index=indices_day_ret_df.columns)
+# indices_day_ret_df.kurtosis()
+KsWR = pd.Series(kurtosis(indices_week_ret_df, fisher=False, axis=0), index=indices_week_ret_df.columns)
+# indices_week_ret_df.kurtosis()
+KlDR = pd.Series(kurtosis(indices_log_day_ret_df, fisher=False, axis=0), index=indices_log_day_ret_df.columns)
+# indices_log_day_ret_df.kurtosis()
+KlWR = pd.Series(kurtosis(indices_log_week_ret_df, fisher=False, axis=0), index=indices_log_week_ret_df.columns)
+# indices_log_week_ret_df.kurtosis()
 
 # Min
 MinsDR = np.min(indices_day_ret_df)
@@ -118,6 +125,33 @@ sDR_parameters = pd.DataFrame(pd.concat([N_table, pd.DataFrame([usDR, VarsDR, Sk
 sWR_parameters = pd.DataFrame(pd.concat([N_table, pd.DataFrame([usWR, VarsWR, SksWR, KsWR, MinsWR, MaxsWR])], axis=1))
 lDR_parameters = pd.DataFrame(pd.concat([N_table, pd.DataFrame([ulDR, VarlDR, SklDR, KlDR, MinlDR, MaxlDR])], axis=1))
 lWR_parameters = pd.DataFrame(pd.concat([N_table, pd.DataFrame([ulWR, VarlWR, SklWR, KlWR, MinlWR, MaxlWR])], axis=1))
+
+"""
+# Histogramme
+indices_day_ret_df['S&PCOMP(RI)'].plot.hist(bins=200)
+plt.show()
+
+IDENTIFICATION QUARTILES
+
+# BOITE A MOUSTACHE
+indices_day_ret_df['S&PCOMP(RI)'].plot.box()
+plt.show()
+# CALC QUARTILES
+Q1 = indices_day_ret_df['S&PCOMP(RI)'].quantile(0.25)
+Q3 = indices_day_ret_df['S&PCOMP(RI)'].quantile(0.75)
+IQR = Q3 - Q1
+coef = IQR*1.5
+# VAL LIMITES
+lim_inf = Q1 - coef
+lim_sup = Q3 + coef
+# RECHERCHE OUTLIERS
+result = indices_day_ret_df[(indices_day_ret_df['S&PCOMP(RI)'] > lim_sup) |
+                            (indices_day_ret_df['S&PCOMP(RI)'] < lim_inf)]
+result['S&PCOMP(RI)'].plot.hist(bins=200)
+plt.show()
+result['S&PCOMP(RI)'].plot.box()
+plt.show()
+"""
 ########################################################################################################################
 
 
@@ -178,12 +212,45 @@ def plot_diff_returns(name, table1, table2, frequency, table_label):
 # Use the function to see the difference for each class of assets
 for x in daily_indices_df.columns:
     # DAILY
-    plot_diff_returns(x, indices_day_ret_df, indices_log_day_ret_df, '∆ Daily returns (percentage points)',
+    plot_diff_returns(x, indices_day_ret_df, indices_log_day_ret_df, '∆ Daily returns',
                       'Simple Returns - Log Returns')
     # WEEKLY
-    plot_diff_returns(x, indices_week_ret_df, indices_log_week_ret_df, '∆ Weekly returns (percentage points)',
+    plot_diff_returns(x, indices_week_ret_df, indices_log_week_ret_df, '∆ Weekly returns',
                       'Simple Returns - Log Returns')
 ########################################################################################################################
+
+
+########################################################################################################################
+"""
+AUTE MOYEN DE MONTRER DIFF ENTE LOG RETURN ET SIMPLE
+
+
+
+def plot2_diff_returns(name, table1, frequency, table1_label, table2):
+    plt.plot(table1.index, table1[name], label=table1_label, color='black')
+
+    top500 = table2[name].nlargest(500)
+    bottom500 = table2[name].nsmallest(500)
+
+    for date, value in top500.items():
+        plt.scatter(date, value, marker='.', color='red')
+
+    for date, value in bottom500.items():
+        plt.scatter(date, value, marker='.', color='red')
+
+    plt.scatter([], [], marker='.', color='red', label='Log Returns')
+    plt.legend()
+
+    setup_plot(name, frequency)
+
+
+for r in daily_indices_df.columns:
+    # Use the function to plot graphs for log daily and logweekly returns
+    plot2_diff_returns(r, indices_day_ret_df, 'Daily returns', 'Simple Returns', indices_log_day_ret_df)
+    plot2_diff_returns(r, indices_week_ret_df, 'Weekly returns', 'Simple Returns', indices_log_week_ret_df)
+"""
+########################################################################################################################
+
 
 ########################################################################################################################
 """
@@ -425,38 +492,40 @@ for i in range(0, 5):
 # JB test (using wikipedia formula: JB = n/6 * (S^2 + (1/4) * (K-3)^2))
 
 JB_test = {'S&PCOMP(RI)': [], 'MLGTRSA(RI)': [], 'MLCORPM(RI)': [], 'WILURET(RI)': [], 'RJEFCRT(TR)': [], 'JPUSEEN': []}
-DW = pd.DataFrame(['Log Daily Returns', 'Log Weekly Returns'])
+DW = pd.DataFrame(['Log Daily Returns JB value', 'Log Daily Returns JB p-value',
+                   'Log Weekly Returns JB value', 'Log Weekly Returns JB p-value'])
 
 
 # Get JB test value and put it into a table
-def JB_log(returns_table, parameters_table, asset_class, alpha, label):
-    JB_log = (len(returns_table)-1) / 6 * (parameters_table[asset_class][2] ** 2 + (1 / 4) * (
+def JB_log(returns_table, parameters_table, asset_class, alpha, label, n_ligne):
+    JB_log = len(returns_table) / 6 * (parameters_table[asset_class][2] ** 2 + (1 / 4) * (
                 parameters_table[asset_class][3] - 3) ** 2)  # length -1 because 1st row is Nan
     k_value = chi2.ppf(1 - alpha, 2)
-    JB_test[asset_class].append(
-        JB_log)  # Becareful, if you don't clear and re-run, data will be added into the table, so size will change
-    if JB_test[asset_class][0] > k_value:
+    p_value = ("{:.4e}".format(1-stats.chi2.cdf(JB_log, 2)))
+    JB_test[asset_class].append(JB_log)
+    JB_test[asset_class].append(p_value)
+    if JB_test[asset_class][n_ligne] >= k_value:
         print('We reject the assumption of normality of', label, 'for', asset_class)
     else:
         print('We do not reject the assumption of normality of', label, 'for', asset_class)
 
 
 # LOG DAILY RETURNS
-JB_log(indices_log_day_ret_df, lDR_parameters, 'S&PCOMP(RI)', 0.05, 'Log Daily Returns')
-JB_log(indices_log_day_ret_df, lDR_parameters, 'MLGTRSA(RI)', 0.05, 'Log Daily Returns')
-JB_log(indices_log_day_ret_df, lDR_parameters, 'MLCORPM(RI)', 0.05, 'Log Daily Returns')
-JB_log(indices_log_day_ret_df, lDR_parameters, 'WILURET(RI)', 0.05, 'Log Daily Returns')
-JB_log(indices_log_day_ret_df, lDR_parameters, 'RJEFCRT(TR)', 0.05, 'Log Daily Returns')
-JB_log(indices_log_day_ret_df, lDR_parameters, 'JPUSEEN', 0.05, 'Log Daily Returns')
+JB_log(indices_log_day_ret_df, lDR_parameters, 'S&PCOMP(RI)', 0.05, 'Log Daily Returns', 0)
+JB_log(indices_log_day_ret_df, lDR_parameters, 'MLGTRSA(RI)', 0.05, 'Log Daily Returns', 0)
+JB_log(indices_log_day_ret_df, lDR_parameters, 'MLCORPM(RI)', 0.05, 'Log Daily Returns', 0)
+JB_log(indices_log_day_ret_df, lDR_parameters, 'WILURET(RI)', 0.05, 'Log Daily Returns', 0)
+JB_log(indices_log_day_ret_df, lDR_parameters, 'RJEFCRT(TR)', 0.05, 'Log Daily Returns', 0)
+JB_log(indices_log_day_ret_df, lDR_parameters, 'JPUSEEN', 0.05, 'Log Daily Returns', 0)
 
 # LOG WEEKLY RETURNS
 print('\n')
-JB_log(indices_log_week_ret_df, lWR_parameters, 'S&PCOMP(RI)', 0.05, 'Log Weekly Returns')
-JB_log(indices_log_week_ret_df, lWR_parameters, 'MLGTRSA(RI)', 0.05, 'Log Weekly Returns')
-JB_log(indices_log_week_ret_df, lWR_parameters, 'MLCORPM(RI)', 0.05, 'Log Weekly Returns')
-JB_log(indices_log_week_ret_df, lWR_parameters, 'WILURET(RI)', 0.05, 'Log Weekly Returns')
-JB_log(indices_log_week_ret_df, lWR_parameters, 'RJEFCRT(TR)', 0.05, 'Log Weekly Returns')
-JB_log(indices_log_week_ret_df, lWR_parameters, 'JPUSEEN', 0.05, 'Log Weekly Returns')
+JB_log(indices_log_week_ret_df, lWR_parameters, 'S&PCOMP(RI)', 0.05, 'Log Weekly Returns', 2)
+JB_log(indices_log_week_ret_df, lWR_parameters, 'MLGTRSA(RI)', 0.05, 'Log Weekly Returns', 2)
+JB_log(indices_log_week_ret_df, lWR_parameters, 'MLCORPM(RI)', 0.05, 'Log Weekly Returns', 2)
+JB_log(indices_log_week_ret_df, lWR_parameters, 'WILURET(RI)', 0.05, 'Log Weekly Returns', 2)
+JB_log(indices_log_week_ret_df, lWR_parameters, 'RJEFCRT(TR)', 0.05, 'Log Weekly Returns', 2)
+JB_log(indices_log_week_ret_df, lWR_parameters, 'JPUSEEN', 0.05, 'Log Weekly Returns', 2)
 
 # Show which JB test values are for daily and which ones are for weekly returns
 JB_test = pd.concat([DW, pd.DataFrame(JB_test)], axis=1)
@@ -467,8 +536,8 @@ JB_test = pd.concat([DW, pd.DataFrame(JB_test)], axis=1)
 """
 3 - A/B : SUMMARY STATISTICS AND CORRELATIONS
 """
-portfolio_day_ret = indices_day_ret_df.mean(axis=1)
-porfolio_week_returns = indices_week_ret_df.mean(axis=1)
+portfolio_day_ret = indices_day_ret_df.mean(axis=1).to_frame(name='Portfolio Daily Returns')
+porfolio_week_returns = indices_week_ret_df.mean(axis=1).to_frame(name='Portfolio Weekly Returns')
 
 # Mean
 usADR = portfolio_day_ret.mean()
@@ -479,12 +548,16 @@ VarsADR = np.var(portfolio_day_ret)
 VarsAWR = np.var(porfolio_week_returns)
 
 # Skewness
-SksADR = portfolio_day_ret.skew()
-SksAWR = porfolio_week_returns.skew()
+SksADR = pd.Series(skew(portfolio_day_ret, axis=0), index=portfolio_day_ret.columns)
+# portfolio_day_ret.skew()
+SksAWR = pd.Series(skew(porfolio_week_returns, axis=0), index=porfolio_week_returns.columns)
+# porfolio_week_returns.skew()
 
 # Kurtosis
-KsADR = portfolio_day_ret.kurtosis()
-KsAWR = porfolio_week_returns.kurtosis()
+KsADR = pd.Series(kurtosis(portfolio_day_ret, fisher=False, axis=0), index=portfolio_day_ret.columns)
+# portfolio_day_ret.kurtosis()
+KsAWR = pd.Series(kurtosis(porfolio_week_returns, fisher=False, axis=0), index=porfolio_week_returns.columns)
+# porfolio_week_returns.kurtosis()
 
 # Min
 MinsADR = np.min(portfolio_day_ret)
@@ -506,54 +579,57 @@ Portfolio_W_parameters = pd.DataFrame(
 # JB test (using wikipedia formula: JB = n/6 * (S^2 + (1/4) * (K-3)^2))
 
 Portfolio_JB_test = {'Portfolio equally weighted': []}
-Portfolio_DW = pd.DataFrame(['Simple Daily Returns', 'Simple Weekly Returns'])
+Portfolio_DW = pd.DataFrame(['Simple Daily Returns JB value', 'Simple Daily Returns JB p-value',
+                             'Simple Weekly Returns JB value', 'Simple Weekly Returns JB p-value'])
 
 
 # Get JB test value and put it into a table
-def Portfolio_JB(returns_table, parameters_table, alpha, label):
-    P_JB = (len(returns_table)-1) / 6 * (parameters_table.iloc[2, 1] ** 2 + (1 / 4) * (
+def Portfolio_JB(returns_table, parameters_table, alpha, label, n_ligne):
+    P_JB = len(returns_table) / 6 * (parameters_table.iloc[2, 1] ** 2 + (1 / 4) * (
                 parameters_table.iloc[3, 1] - 3) ** 2)  # length -1 because 1st row is Nan
     k_value = chi2.ppf(1 - alpha, 2)
-    Portfolio_JB_test['Portfolio equally weighted'].append(
-        P_JB)  # Becareful, if you don't clear and re-run, data will be added into the table, so size will change
-    if Portfolio_JB_test['Portfolio equally weighted'][0] > k_value:
+    p_value = ("{:.4e}".format(1 - stats.chi2.cdf(P_JB, 2)))
+    Portfolio_JB_test['Portfolio equally weighted'].append(P_JB)
+    Portfolio_JB_test['Portfolio equally weighted'].append(p_value)
+    if Portfolio_JB_test['Portfolio equally weighted'][n_ligne] >= k_value:
         print('We reject the assumption of normality of', label, 'for the equally weighted portfolio.')
     else:
         print('We do not reject the assumption of normality of', label, 'for the equally weighted portfolio.')
 
 
 # SIMPLE DAILY RETURNS
-Portfolio_JB(portfolio_day_ret, Portfolio_D_parameters, 0.05, 'Simple Daily Returns')
+Portfolio_JB(portfolio_day_ret, Portfolio_D_parameters, 0.05, 'Simple Daily Returns', 0)
 
 # LOG WEEKLY RETURNS
-Portfolio_JB(porfolio_week_returns, Portfolio_W_parameters, 0.05, 'Simple Weekly Returns')
+Portfolio_JB(porfolio_week_returns, Portfolio_W_parameters, 0.05, 'Simple Weekly Returns', 2)
 
 # Show which JB test values are for daily and which ones are for weekly returns of the portfolio
 Portfolio_JB_test = pd.concat([Portfolio_DW, pd.DataFrame(Portfolio_JB_test)], axis=1)
 ########################################################################################################################
-
+portfolio_day_ret['Portfolio Daily Returns'].plot.hist(bins=200)
+plt.show()
 
 ########################################################################################################################
 """
-EXTRACT TABLES
+# EXTRACT TABLES
 
 sDR_parameters = sDR_parameters.set_index(0)
 sWR_parameters = sWR_parameters.set_index(0)
 lDR_parameters = lDR_parameters.set_index(0)
 lWR_parameters = lWR_parameters.set_index(0)
 JB_test = JB_test.set_index(0)
-Portfolio_W_parameters.set_index(0)
-Portfolio_D_parameters.set_index(0)
-Portfolio_JB_test.set_index(0)
+Portfolio_W_parameters = Portfolio_W_parameters.set_index(0)
+Portfolio_D_parameters = Portfolio_D_parameters.set_index(0)
+Portfolio_JB_test = Portfolio_JB_test.set_index(0)
 
 
 with pd.ExcelWriter('Outputs/tabl_extratct.xlsx') as writer:
-    sDR_parameters.to_excel(writer, sheet_name='Feuil 1')
-    sWR_parameters.to_excel(writer, sheet_name='Feuil 2')
-    lDR_parameters.to_excel(writer, sheet_name='Feuil 3')
-    lWR_parameters.to_excel(writer, sheet_name='Feuil 4')
-    JB_test.to_excel(writer, sheet_name='Feuil 5')
-    Portfolio_W_parameters.to_excel(writer, sheet_name='Feuil 6')
-    Portfolio_D_parameters.to_excel(writer, sheet_name='Feuil 7')
-    Portfolio_JB_test.to_excel(writer, sheet_name='Feuil 8')
+    sDR_parameters.to_excel(writer, sheet_name='Feuil1')
+    sWR_parameters.to_excel(writer, sheet_name='Feuil2')
+    lDR_parameters.to_excel(writer, sheet_name='Feuil3')
+    lWR_parameters.to_excel(writer, sheet_name='Feuil4')
+    JB_test.to_excel(writer, sheet_name='Feuil5')
+    Portfolio_W_parameters.to_excel(writer, sheet_name='Feuil6')
+    Portfolio_D_parameters.to_excel(writer, sheet_name='Feuil7')
+    Portfolio_JB_test.to_excel(writer, sheet_name='Feuil8')
 """
